8c2283269284f7853286c82ee7b6d08b2f46f43a
Madina <madinadzanaeva670@gmail.com>
2025-12-16 02:41:52 +0300

    Инициализация проекта

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      // Объекты переменные
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Task Book</h1>
+</template>
+
+<style scoped>
+</style>
